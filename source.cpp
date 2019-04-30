1d3c17e778df11fae55c1acb32fa792429bb86e2
tmcsherryyy <47239580+tmcsherryyy@users.noreply.github.com>
2019-04-30 15:55:45 +0800

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,59 +1,21 @@
 #include <iostream>
+#include <string>
+#include <chrono>
+#include <cmath>
 using namespace std;
 
 const int tablesize=25, tablenum=25;
 
-//initialise() initialise the array table
+struct set{
+    int ava, noc, time;
+};
+
+
+//to initialise the array table
 //Tables which are available for customers store 0
 //Tables which are occupied store 1
 //others store -1
-void initialise(int table[tablesize][tablenum]){
-    
-    for (int i=0; i<tablesize; i++){
-        for (int j=0; j<tablenum; j++){
-            table[i][j]=-1;
-            
-            //initialise table of 2//
-            if (i==2){
-                if (j<4){
-                    table[i][j]=0;
-                }
-            }
-            
-            //initialise table of 4//
-            if (i==4){
-                if (j<4){
-                    table[i][j]=0;
-                }
-            }
-            
-            //initialise table of 6//
-            if (i==6){
-                if (j<2){
-                    table[i][j]=0;
-                }
-            }
-            
-            //initialise table of 8//
-            if (i==8){
-                if (j<2){
-                    table[i][j]=0;
-                }
-            }
-            
-            //initialise table of 12//
-            if (i==12){
-                if (j<1){
-                    table[i][j]=0;
-                }
-            }
-            
-            }
-    }
-    
-}
-
-void setting(int table[tablesize][tablenum], int price){
+void setting(set table[tablesize][tablenum], int price){
     
     //Reset the price per head
     cout << "Price per head is : ";
@@ -63,7 +25,7 @@ void setting(int table[tablesize][tablenum], int price){
     int size,num;
     for (int i=0; i<tablesize; i++){
         for (int j=0; j<tablenum; j++){
-            table[i][j]=-1;
+            table[i][j].ava=-1;
         }
     }
     
@@ -75,26 +37,146 @@ void setting(int table[tablesize][tablenum], int price){
         cout << "Number of table of " << size << " is : ";
         cin >> num;
         for (int i=0; i<num; i++){
-            table[size][i]=0;
+            table[size][i].ava=0;
         }
-        cout << "Table size of : ";
+        cout << "Table size of (Enter 0 to finish setting) : ";
         cin >> size;
+    }
+}
+
+
+void newtable(set table[tablesize][tablenum]){
+    
+    int num;
+    cout << "Number of customers : ";
+    cin >> num;
+    if (num>tablesize)
+        cout << "No suitable table for " << num << " customers" << endl;
+    
+    //find a suitable table
+    for (int i=num; i<tablesize; i++){
+        
+        //check the smallest table size for number of customers input
+        if (table[i][0].ava != -1){
+            
+            //check if there is table available
+            for (int j=0; j<tablenum; j++){
+                
+                //return table number that waiter should bring customers to
+                if (table[i][j].ava == 0){
+                    cout << "Please bring them to table " << i << j << " " << endl;
+                    table[i][j].noc = num ;
+                    table[i][j].ava = 1 ;
+                    
+                    
+                    break;
+                }
+                //return "Pleaese wait!" when there is no table available
+                else if ((table[i][j].ava == 1) && (table[i][j+1].ava == -1)){
+                    cout << "Please wait!" << endl;
+                }
+                
+            }
+            break;
+        }
+        if (i == tablesize-1)
+            cout << "No suitable table for " << num << " customers" << endl;
     }
     
 }
 
-int main(){
-    int table[tablesize][tablenum];
-    int price=180;
-    initialise(table);
+
+int seperatetableno(int num, int &i, int &j){
     
-    string userinput;
-    //search,occupy,release,setting
-    cout << "Operate : ";
-    cin >> userinput;
-    if (userinput == "setting")
-        setting(table,price);
-    
-    return 0;
+    if (num<100){
+        i = num/10;
+        j = num%10;
+    }
+    else if (num>99){
+        i = num/100;
+        j = num%10;
+    }
+    return i;
 }
 
+
+void release(set table[tablesize][tablenum], int price){
+    
+    int num,i,j;
+    cout << "Table number : ";
+    cin >> num;
+    seperatetableno(num,i,j);
+    
+    cout << "------------------" << endl << "RECEIPT" << endl;
+    cout << "Number of customers : " << table[i][j].noc << endl;
+    cout << "Net total before service charge: " << price*table[i][j].noc << endl;
+    cout << "Total amount = " << price*table[i][j].noc*1.1 << endl << "------------------" << endl;
+    
+    table[i][j].noc = 0;
+    table[i][j].ava = 0;
+    table[i][j].time = 0;
+}
+
+
+void searchtable(set table[tablesize][tablenum]){
+    
+    int num,i,j;
+    cout << "Table number : ";
+    cin >> num;
+    seperatetableno(num,i,j);
+    
+    if (table[i][j].ava == 1){
+        cout << "This table is occupied" << endl;
+        cout << "Number of customers of this table : " << table[i][j].noc << endl;
+        cout << "Time remaining for this table : " << endl;
+    }
+    else if (table[i][j].ava == 0)
+        cout << "This table is available for " << i << endl;
+    else if (table[i][j].ava == -1)
+        cout << "Table number doesn't exist!" <<  endl;
+    
+}
+
+
+int main(){
+    set table[tablesize][tablenum];
+    int price=180;
+    initialise(table); //DEL
+    // setting(table;price);
+    
+    
+    int userinput;
+    cout << endl;
+    cout << "(1:New Table ; 2:Bill & Release Table ; 3:Search Table ; 4:Reset ; 9:Quit Program)" << endl << "Operate : " ;
+    cin >> userinput;
+    cout << endl;
+    
+    while (userinput != 9){
+        switch (userinput) {
+            case 1:
+                newtable(table);
+                break;
+            
+            case 2:
+                release(table,price);
+                break;
+                
+            case 3:
+                searchtable(table);
+                break;
+                
+            case 4:
+                setting(table,price);
+                break;
+
+        }
+        cout << endl;
+        cout << "(1:New Table ; 2:Bill & Release Table ; 3:Search Table ; 4:Reset ; 9:Quit Program)" << endl << "Operate : " ;
+        cin >> userinput;
+        cout << endl;
+        
+    }
+    
+    cout << "System turning off..." <<endl;
+    return 0;
+}
